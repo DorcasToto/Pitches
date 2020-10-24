271981fe6ef09bacec338a96b09889b12060cef4
Moringa School <dorcasmosonik@gmail.com>
2020-10-24 16:32:00 +0300

    user authentication

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from . import db
+from werkzeug.security import generate_password_hash,check_password_hash
 
 class User(db.Model):
     __tablename__ = 'users'
@@ -9,6 +10,19 @@ class User(db.Model):
     biography = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password = db.Column(db.String(255))
+
+    @property
+    def password(self):
+
+        raise AttributeError('You cannot read the password attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password = generate_password_hash(password)
+
+
+    def verify_password(self,password):
+        return check_password_hash(self.password,password)
 
 
     def __repr__(self):
