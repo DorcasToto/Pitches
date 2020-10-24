f3e4e0d4f5d6c11d0822ace7958befd46c61b76d
Moringa School <dorcasmosonik@gmail.com>
2020-10-24 21:43:26 +0300

    profile page

--- app/main/views.py
+++ app/main/views.py
@@ -4,15 +4,17 @@ from . import main
 from ..models import User
 from .. import db
 from flask_login import login_required,current_user
-import datetime
 
 @main.route('/')
 def index():
 
-    '''
-    View root page function that returns the index page and its data
-    '''
+    return render_template('index.html')
 
-    title = 'Home '
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
 
-    return render_template('index.html',title = title)
+    return render_template('profile/profile.html',user = user)    
+
