60c91dc89c885610c8c17b4815c514df2369c9fc
Moringa School <dorcasmosonik@gmail.com>
2020-10-24 17:19:21 +0300

    user registration

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 
+from flask_login import LoginManager
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
@@ -16,6 +22,7 @@ def create_app(config_name):
     # Initializing flask extensions
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
